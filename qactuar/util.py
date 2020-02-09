3a462ef2f6c3d6010106ca4f993cbe4c929b23c0
Anthony Post <anthony.post@q2ebanking.com>
2020-02-09 13:22:23 -0600

    add tornado wrapper

--- qactuar/util.py
+++ qactuar/util.py
@@ -119,3 +119,138 @@ def parse_proc_mem_line(mem_line: str) -> MemStat:
                 setattr(mem_stat, mem_stat_map[key], int(value))
 
     return mem_stat
+
+
+try:
+    import tornado
+except ImportError:
+    pass
+else:
+    from typing import Type
+
+    from tornado.http1connection import HTTP1Connection
+    from tornado.httputil import (
+        HTTPServerRequest, HTTPHeaders, RequestStartLine,
+    )
+    from tornado.iostream import BaseIOStream
+    from tornado.web import Application, RequestHandler
+
+    from qactuar.logs import create_access_logger, create_child_logger
+
+
+    class QactuarStream(BaseIOStream):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def close_fd(self):
+            pass
+
+        def write_to_fd(self, data: memoryview) -> int:
+            try:
+                return len(data.tobytes())
+            finally:
+                del data
+
+        def read_from_fd(self, buf):
+            return len(buf)
+
+        def fileno(self):
+            return 0
+
+
+    def to_bytes(value):
+        return str(value).encode('utf-8')
+
+
+    class TornadoWrapper:
+        def __init__(self, tornado_handler: Type[RequestHandler]):
+            self.logger = create_child_logger()
+            self.access_logger = create_access_logger()
+            self.scope = {}
+            self.send = None
+            self.receive = None
+            self.handler_type = tornado_handler
+
+        async def __call__(self, scope, receive, send):
+            # noinspection PyAbstractClass
+            class QactuarHandler(self.handler_type):
+                def __init__(self, a, r, logger, **kwargs):
+                    super().__init__(a, r, **kwargs)
+                    self._qactuar_body = []
+                    self._qactuar_headers = []
+                    self._logger = logger
+
+                def write(self, chunk):
+                    super().write(chunk)
+                    self._qactuar_body.append(to_bytes(chunk))
+
+                def add_header(self, name, value):
+                    super().add_header(name, value)
+                    self._qactuar_headers.append((to_bytes(name), to_bytes(value)))
+
+                def set_header(self, name, value):
+                    super().set_header(name, value)
+                    self._qactuar_headers.append((to_bytes(name), to_bytes(value)))
+
+            if scope["type"] == 'http':
+                self.scope = scope
+                self.receive = receive
+                self.send = send
+                received = await receive()
+                body = received['body']
+                headers = HTTPHeaders({
+                    header[0].decode('utf-8'): header[1].decode('utf-8')
+                    for header in scope['headers']
+                })
+                request_start_line = RequestStartLine(
+                    scope['method'],
+                    scope['path'],
+                    scope['http_version']
+                )
+                http_connection = HTTP1Connection(QactuarStream(), False)
+                http_connection._request_start_line = request_start_line
+                http_connection._request_headers = headers
+                request = HTTPServerRequest(
+                    method=scope['method'],
+                    uri=scope['path'],
+                    version=scope['http_version'],
+                    headers=headers,
+                    body=body,
+                    host=scope['server'][0],
+                    connection=http_connection,
+                    start_line=request_start_line,
+                )
+                handler = QactuarHandler(Application(), request, self.logger, **{
+                    'logging_level': 'DEBUG'
+                })
+                handler._transforms = []
+                handler.application.transforms = []
+                method_map = {
+                    'GET': handler.get,
+                    'POST': handler.post,
+                    'PUT': handler.put,
+                    'DELETE': handler.delete,
+                    'OPTION': handler.options,
+                    'HEAD': handler.head,
+                    'PATCH': handler.patch,
+                }
+                try:
+                    await method_map[scope['method']]()
+                except Exception as err:
+                    self.access_logger.exception(err)
+                    result = b"500 Server Error"
+                    handler.set_status(500)
+                else:
+                    result = b"".join(handler._qactuar_body)
+                status = str(handler.get_status())
+                response_headers = handler._qactuar_headers
+                await send({
+                    "type": "http.response.start",
+                    "status": status,
+                    "headers": response_headers
+                })
+                await send({
+                    "type": "http.response.body",
+                    "body": result,
+                    "more_body": False
+                })
